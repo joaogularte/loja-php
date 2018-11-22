4fec0fd6ccbf8e73df20955ce2299bfbcbb5a7a8
joaogularte <jov96@hotmail.com>
2018-11-22 08:47:08 -0200

    :)

--- models/banco-usuario.php
+++ models/banco-usuario.php
@@ -2,7 +2,11 @@
 
     function consultaUsuario($conexao, $email, $senha){
         $senhaMd5 = md5($senha);
+        $email = mysqli_real_escape_string($conexao, $email);
+        var_dump($email);
         $query = "select * from usuario where email='{$email}' and senha='{$senhaMd5}'";
+        var_dump($query);
+    
         $resultado = mysqli_query($conexao, $query);
         $usuario = mysqli_fetch_assoc($resultado);
         return $usuario;
